8c099ba6d2ea308373c4eed28126d870a96a42cc
Clickbaithate <gg.guzzman1514@gmail.com>
2024-07-07 16:59:27 -0500

    Adding Data like Colors and List of Series

---
+++ lib/data/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Color(0xffFFFFFF);
+const Color blackColor = Color(0xff000000);
+const Color blueColor = Color(0xff0075FF);
+const Color redColor = Color(0xffFF0000);
+const Color biegeColor = Color(0xffB99976);
