5f19221099febc093cf8bdd0bd59ceb826f16935
Clickbaithate <gg.guzzman1514@gmail.com>
2024-07-08 02:10:49 -0500

    Starting to Prepare To Use RiverPod State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:funko_vault/screens/bottom_nav.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +12,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: "FunkoVault",
+      home: BottomNav()
     );
   }
 }
