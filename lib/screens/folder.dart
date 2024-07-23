fea832101c603c7dae9e0b0bb42f2cafcf7c33be
Clickbaithate <gael.guzman01@utrgv.edu>
2024-07-22 19:21:42 -0500

    Added Folder Screen, Just Blank

---
+++ lib/screens/folder.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FolderPage extends ConsumerWidget {
+  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FunkoVault"),
+      ),
+      body: Center(
+        child: Text("Folders Page"),
+      ),
+    );
+
+  }
+
+}
