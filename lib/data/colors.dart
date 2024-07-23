d1bf03f5f009503d811a11e1bd2205c6ed2ab53b
Clickbaithate <gg.guzzman1514@gmail.com>
2024-07-23 17:25:03 -0500

    Just changed a color

--- lib/data/colors.dart
+++ lib/data/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color whiteColor = Color(0xffFFFFFF);
 const Color blackColor = Color(0xff000000);
-const Color grayColor = Color(0xffCCD1CF);
+const Color grayColor = Color(0xffEAE7EA);
 const Color blueColor = Color(0xff0075FF);
 const Color redColor = Color(0xffFF0000);
 const Color biegeColor = Color(0xffB99976);
