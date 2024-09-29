d8e92f456141cde3a0646eed58c03d36ceef10ca
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-29 12:18:54 -0500

    Added fade in effect, tweaked some colors

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:funko_vault/components/funko_card.dart';
 import 'package:funko_vault/data/colors.dart';
+import 'package:funko_vault/screens/search_page.dart';
 import 'package:funko_vault/services/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
-// Class Definition
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
@@ -15,21 +15,31 @@ class HomePage extends ConsumerStatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-
 class _HomePageState extends ConsumerState<HomePage> {
   
   // Class Variables
   final ScrollController _scrollController = ScrollController();
   final formatter = NumberFormat('#,###');
+  bool _isLoadingDelayed = true;
+  bool _isContentVisible = false;
 
   // Override Methods
   @override
   void initState() {
     super.initState();
+    _startDelay();
     _scrollController.addListener(() {
       if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        ref.read(funkoListProvider.notifier).fetchFunkos();
+        ref.read(homeFunkoListProvider.notifier).fetchFunkos();
       }
+    });
+  }
+
+  void _startDelay() async {
+    await Future.delayed(Duration(seconds: 0));
+    setState(() {
+      _isLoadingDelayed = false;
+      _isContentVisible = true;
     });
   }
 
@@ -41,77 +51,96 @@ class _HomePageState extends ConsumerState<HomePage> {
 
   // Helper Methods
   Future<void> refresh() async {
-    await ref.read(funkoListProvider.notifier).fetchFunkos(refresh: true);
+    await ref.read(homeFunkoListProvider.notifier).fetchFunkos(refresh: true);
   }
 
   // Build Method
   @override
   Widget build(BuildContext context) {
-
-    // Fetching Funkos
-    final asyncFunkos = ref.watch(funkoListProvider);
+    // Fetching Funkos after delay
+    final asyncFunkos = _isLoadingDelayed ? AsyncValue.loading() : ref.watch(homeFunkoListProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "FunkoVault",
+          "iFunko",
           style: GoogleFonts.fredoka(fontSize: 36, color: blackColor, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: whiteColor,
         actions: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+            padding: const EdgeInsets.fromLTRB(0, 0, 16, 0),
             child: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.search, size: 32),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SearchPage()),
+                );
+              },
+              icon: Icon(Icons.search, size: 32, color: blackColor),
             ),
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
-            child: asyncFunkos.when(
-              data: (funkos) {
-                final num = funkos.length;
-                return Text(
-                  "${formatter.format(num)} out of 49,000",
-                  style: GoogleFonts.fredoka(fontSize: 12, color: blackColor, fontWeight: FontWeight.bold),
-                );
-              },
-              loading: () => Text("Loading..."),
-              error: (e, stack) => Text("Error!"),
-            ),
-          ),
-          Expanded(
-            child: RefreshIndicator(
-              onRefresh: refresh,
-              child: asyncFunkos.when(
-                data: (funkos) => funkos.isNotEmpty ? GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 7 / 10,
-                  ),
-                  controller: _scrollController,
-                  physics: AlwaysScrollableScrollPhysics(),
-                  itemCount: funkos.length,
-                  itemBuilder: (context, index) {
-                    final funko = funkos[index];
-                    return FunkoCard(funko: funko);
+      body: AnimatedOpacity(
+        opacity: _isContentVisible ? 1.0 : 0.0,
+        duration: Duration(seconds: 1),
+        child: Container(
+          color: whiteColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
+                child: asyncFunkos.when(
+                  data: (funkos) {
+                    final num = funkos.length;
+                    return Text(
+                      "${formatter.format(num)} out of 49,000",
+                      style: GoogleFonts.fredoka(fontSize: 12, color: blackColor, fontWeight: FontWeight.bold),
+                    );
                   },
-                ) : ListView(
-                  controller: _scrollController,
-                  physics: AlwaysScrollableScrollPhysics(),
-                  children: [Center(child: Text("No data available! Pull to refresh."))]
+                  loading: () => Text(
+                      "Loading...",
+                      style: GoogleFonts.fredoka(fontSize: 12, color: blackColor, fontWeight: FontWeight.bold),
+                    ),
+                  error: (e, stack) => Text("Error!"),
                 ),
-                loading: () => Center(child: CircularProgressIndicator()),
-                error: (e, stack) => Text("Error!")
               ),
-            ),
+              Expanded(
+                child: Container(
+                  color: whiteColor,
+                  child: RefreshIndicator(
+                    onRefresh: refresh,
+                    child: asyncFunkos.when(
+                      data: (funkos) => funkos.isNotEmpty ? GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 7 / 10,
+                        ),
+                        controller: _scrollController,
+                        physics: AlwaysScrollableScrollPhysics(),
+                        itemCount: funkos.length,
+                        itemBuilder: (context, index) {
+                          final funko = funkos[index];
+                          return FunkoCard(funko: funko);
+                        },
+                      ) : ListView(
+                        controller: _scrollController,
+                        physics: AlwaysScrollableScrollPhysics(),
+                        children: [Center(child: Text("No data available! Pull to refresh."))],
+                      ),
+                      loading: () => Center(child: CircularProgressIndicator()),
+                      error: (e, stack) => Text("Error!"),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
