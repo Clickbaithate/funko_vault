3305bd806a5dba72aa26f82ef66889722695b385
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-29 12:04:41 -0500

    Added the folder colors and tweaked some of the series.

---
+++ lib/data/folder_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorsList = [
+  Color(0xff64ABFF),
+  Color(0xffFF8787),
+  Color(0xffFDA3FF),
+
+  Color(0xffEFFF8F),
+  Color(0xffAAFFF5),
+  Color(0xffFFB287),
+
+  Color(0xffFF6464),
+  Color(0xffFF9264),
+  Color(0xffB1FF64),
+  
+  Color(0xff6499FF),
+  Color(0xff9664FF),
+  Color(0xffC464FF),
+  
+  Color(0xffFF64E6),
+  Color(0xffFF6489),
+];
