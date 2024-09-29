744c28b16508668d78277b0d358c9a0448f0c84b
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-29 12:15:20 -0500

    Updated folder_items to include search function, and displaying the funkos that belong in that folder.

--- lib/screens/folder_items.dart
+++ lib/screens/folder_items.dart
@@ -1,18 +1,130 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: avoid_print, prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:funko_vault/components/funko_card.dart';
+import 'package:funko_vault/data/colors.dart';
+import 'package:funko_vault/models/folder.dart';
+import 'package:funko_vault/models/funko.dart';
+import 'package:funko_vault/services/database.dart';
 
-class FolderItemPage extends ConsumerWidget {
+class FolderItems extends StatefulWidget {
 
-  const FolderItemPage({super.key});
-  
+  final Folder folder;
+  const FolderItems({required this.folder, super.key});
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<FolderItems> createState() => _FolderItemsState();
+
+}
+
+class _FolderItemsState extends State<FolderItems> {
+
+  List<Funko> allFunkos = [];
+  List<Funko> filteredFunkos = [];
+  TextEditingController searchController = TextEditingController();
+  final DatabaseService _databaseService = DatabaseService.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCollectedFunkos();
+  }
+
+  // filtering funkos
+  void filterCollections() {
+    setState(() {
+      String searchQuery = searchController.text.toLowerCase();
+      if (searchQuery.isEmpty) {
+        filteredFunkos = List.from(allFunkos);
+      } else {
+        filteredFunkos = allFunkos.where((funko) {
+          return funko.name.toLowerCase().contains(searchQuery);
+        }).toList();
+      }
+    });
+  }
+
+  // fetching all funkos that belong in this folder
+  Future<void> fetchCollectedFunkos() async {
+    try {
+      List<Funko> collectedFunkos = await _databaseService.getSavedFunkos(widget.folder.name);
+      print("Funko Collected: ${widget.folder.name}");
+      setState(() {
+        allFunkos = collectedFunkos;
+        filteredFunkos = List.from(allFunkos);
+      });
+    } catch (e) {
+      print("Error fetching liked Funkos: $e");
+    }
+  }
+
+  // Meant to update state based on these actions but unsure if these work
+  void handleLiked() {
+    fetchCollectedFunkos();
+  }
+  void handleDeleted() {
+    fetchCollectedFunkos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("FunkoVault"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130),
+        child: AppBar(
+          title: Text(
+            widget.folder.name,
+            style: TextStyle(
+              color: blackColor,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          scrolledUnderElevation: 0,
+          backgroundColor: whiteColor,
+          elevation: 0,
+          flexibleSpace: Column(
+            children: [
+              SizedBox(height: 80), // Adjust height to give more space
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: SizedBox(
+                  height: 54,
+                  child: Center(
+                    child: TextField(
+                      controller: searchController,
+                      onChanged: (value) {
+                        filterCollections();
+                      },
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        hintText: "Search...",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: grayColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(child: Text("Folder Item Page"))
+      // if no funkos then message saying that, otherwise build gridview showing them all
+      body: filteredFunkos.isEmpty ? Container(color: whiteColor, child: Center(child: Text("This list is empty!"))) : Container(
+        color: whiteColor,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 7 / 10),
+          itemCount: filteredFunkos.length,
+          itemBuilder: (context, index) {
+            return FunkoCard(funko: filteredFunkos[index]);
+          },
+        ),
+      ),
     );
   }
 }
