7f2901ad741b475b03e707dc7fcce74ef29a2eb3
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-29 12:08:15 -0500

    Added a fade in effect to hide the lag from the initial fetch, lag might just be my emulator though.

--- lib/screens/bottom_nav.dart
+++ lib/screens/bottom_nav.dart
@@ -16,6 +16,7 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
 
   int _currentPageIndex = 1;
+  bool _isContentVisible = false;
 
   final List<Widget> _screens = [
     FolderPage(),
@@ -26,60 +27,76 @@ class _BottomNavState extends State<BottomNav> {
   @override
   void initState() {
     super.initState();
+    _startDelay();
+  }
+
+  void _startDelay() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _isContentVisible = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      body: IndexedStack(
-        index: _currentPageIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: Container(
-        margin: EdgeInsets.all(30),
-        decoration: BoxDecoration(
-          color: whiteColor,
-          borderRadius: BorderRadius.circular(24),
-          boxShadow: const [
-            BoxShadow(
-              color: blackColor,
-              blurRadius: 10,
-              offset: Offset(0, 4)
-            )
-          ]
+      body: AnimatedOpacity(
+        opacity: _isContentVisible ? 1.0 : 0.0, 
+        duration: Duration(seconds: 1),
+        child: IndexedStack(
+          index: _currentPageIndex,
+          children: _screens,
         ),
-        child: NavigationBarTheme(
-          data: NavigationBarThemeData(
-            backgroundColor: Colors.transparent,
-            indicatorColor: Colors.transparent,
-            indicatorShape: CircleBorder()
-          ),
-          child: NavigationBar(
-            selectedIndex: _currentPageIndex,
-            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-            indicatorColor: Colors.transparent,
-            onDestinationSelected: (index) => setState(() => _currentPageIndex = index),
-            destinations: [
-              NavigationDestination(
-                icon: Icon(Icons.folder_outlined, color: biegeColor.withOpacity(0.3), size: 40),
-                selectedIcon: Icon(Icons.folder, color: biegeColor, size: 40),
-                label: "Folders Page",
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.home_outlined, color: blueColor.withOpacity(0.3), size: 40),
-                selectedIcon: Icon(Icons.home, color: blueColor, size: 40),
-                label: "Home Page",
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.favorite_outline, color: redColor.withOpacity(0.3), size: 40),
-                selectedIcon: Icon(Icons.favorite, color: redColor, size: 40),
-                label: "Liked Page",
+      ),
+      bottomNavigationBar: AnimatedOpacity(
+        opacity: _isContentVisible ? 1.0 : 0.0, 
+        duration: Duration(seconds: 1),
+        child: Container(
+          margin: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            color: whiteColor,
+            borderRadius: BorderRadius.circular(24),
+            boxShadow: const [
+              BoxShadow(
+                color: blackColor,
+                blurRadius: 10,
+                offset: Offset(0, 4),
               )
             ],
           ),
+          child: NavigationBarTheme(
+            data: NavigationBarThemeData(
+              backgroundColor: Colors.transparent,
+              indicatorColor: Colors.transparent,
+              indicatorShape: CircleBorder(),
+            ),
+            child: NavigationBar(
+              selectedIndex: _currentPageIndex,
+              labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+              indicatorColor: Colors.transparent,
+              onDestinationSelected: (index) => setState(() => _currentPageIndex = index),
+              destinations: [
+                NavigationDestination(
+                  icon: Icon(Icons.folder_outlined, color: biegeColor.withOpacity(0.3), size: 40),
+                  selectedIcon: Icon(Icons.folder, color: biegeColor, size: 40),
+                  label: "Folders Page",
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.home_outlined, color: blueColor.withOpacity(0.3), size: 40),
+                  selectedIcon: Icon(Icons.home, color: blueColor, size: 40),
+                  label: "Home Page",
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.favorite_outline, color: redColor.withOpacity(0.3), size: 40),
+                  selectedIcon: Icon(Icons.favorite, color: redColor, size: 40),
+                  label: "Liked Page",
+                ),
+              ],
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
 }
